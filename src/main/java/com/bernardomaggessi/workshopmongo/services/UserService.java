65e0038c6adac12dc65e0a39f3752585ccb44152
bernardomaggessi <bermaggessi2@yahoo.com>
2024-12-16 17:38:32 -0300

    obtendo usuário por id e tratando exceptions

--- src/main/java/com/bernardomaggessi/workshopmongo/services/UserService.java
+++ src/main/java/com/bernardomaggessi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.bernardomaggessi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bernardomaggessi.workshopmongo.domain.User;
 import com.bernardomaggessi.workshopmongo.repository.UserRepository;
+import com.bernardomaggessi.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User fundById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
 	}
 }
