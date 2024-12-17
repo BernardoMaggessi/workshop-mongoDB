d6df3e13ba0df5db88036d735ea91acff93b2cef
bernardomaggessi <bermaggessi2@yahoo.com>
2024-12-17 16:33:43 -0300

    atualização de usuário com PUT

--- src/main/java/com/bernardomaggessi/workshopmongo/services/UserService.java
+++ src/main/java/com/bernardomaggessi/workshopmongo/services/UserService.java
@@ -25,13 +25,27 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	
 	public User fromDto(UserDTO dto) {
 		return new User(dto.getId(),dto.getName(),dto.getEmail());
 	}
+	
 	public void delete(String id) {
 		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());	
 	}
 }
