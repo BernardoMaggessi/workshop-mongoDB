0abc8e0da6cba5745fa5e3921c8dc2ca1bbe3288
bernardomaggessi <bermaggessi2@yahoo.com>
2025-01-06 09:34:11 -0300

    Consultas simples com QUERY METHODS

--- src/main/java/com/bernardomaggessi/workshopmongo/resources/PostResource.java
+++ src/main/java/com/bernardomaggessi/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.bernardomaggessi.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bernardomaggessi.workshopmongo.domain.Post;
+import com.bernardomaggessi.workshopmongo.resources.utils.URL;
 import com.bernardomaggessi.workshopmongo.services.PostService;
 
 @RestController
@@ -22,6 +26,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+	//http://localhost:8080/posts/titlesearch?text=bom%20dia
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 
 }
--- src/main/java/com/bernardomaggessi/workshopmongo/services/PostService.java
+++ src/main/java/com/bernardomaggessi/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.bernardomaggessi.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	//http://localhost:8080/posts/titlesearch?text=bom%20dia
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
